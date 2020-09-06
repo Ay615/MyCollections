a564f2258f3bfa2ec3cfba7c866ed544af1736d6
Aynigar <aynigar615@gmail.com>
2020-09-06 19:45:20 +0300

    MultiThread

--- src/main/java/com/selenium/tests/GoogleMultiThreadTest.java
+++ src/main/java/com/selenium/tests/GoogleMultiThreadTest.java
@@ -5,13 +5,19 @@ public class GoogleMultiThreadTest {
         GoogleMultiThread search1 = new GoogleMultiThread("Java");
         GoogleMultiThread search2 = new GoogleMultiThread("Selenium");
         GoogleMultiThread search3 = new GoogleMultiThread("Test Automation");
+        GoogleMultiThread search4=new GoogleMultiThread("Test Engineer");
+
         //define each thread
         Thread browser1 = new Thread(search1, "Browser 1");
         Thread browser2 = new Thread(search2, "Browser 2");
         Thread browser3 = new Thread(search3, "Browser 3");
+        Thread browser4 = new Thread(search3, "Browser 4");
+
         //execute the thread
         browser1.start();
         browser2.start();
         browser3.start();
+        browser4.start();
+
     }
 }
